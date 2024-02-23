c3e7257c3ed3debfb93491bf3b1ee18a4c34836e
willisman31 <jake22032@gmail.com>
2024-02-22 23:53:56 -0500

    Bug fixes and extensibility functions

--- templates/github-template.sh
+++ templates/github-template.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # Create github-template
 
+base_dir="$(dirname $0)/.."
+
 project=$1
 mkdir "./$project"
 
 printf "# $1\n\nA new project using the C programming language\n" | cat > ./$project/README.md
-cat ./LICENSE | cat > "./$project/LICENSE"
+cat $base_dir/artifacts/LICENSE | cat > "./$project/LICENSE"
 
-sed -i '' -e "s/Jacob Willis/-/g" ./$project/LICENSE 2>/dev/null
+sed -i '' -e "s/ERASEME/$(whoami)/g" ./$project/LICENSE 2>/dev/null
 
 printf "" 2>/dev/null
 
--- templates/shaw-template.sh
+++ templates/shaw-template.sh
@@ -3,15 +3,17 @@
 
 project=$1
 mkdir "./$project"
+base_dir="$(dirname $0)/.."
 
 printf "# $1\n\nA new project using the C programming language\n\nTemplate modeled after an early example from Zed Shaw's *Learn C The hard Way*\n" | cat > ./$project/README.md
-cat ./LICENSE | cat > "./$project/LICENSE"
-sed -i '' -e "s/Jacob Willis/-/g" ./$project/LICENSE 2>/dev/null
+
+cat $base_dir/artifacts/LICENSE | cat > "./$project/LICENSE"
+sed -i '' -e "s/ERASEME/$(whoami)/g" ./$project/LICENSE 2>/dev/null
 
 mkdir "./$project/src"
 mkdir "./$project/bin"
 mkdir "./$project/test"
 
-cp ./artifacts/shaw/dbg.h "./$project/src/"
-cp ./artifacts/shaw/Makefile "./$project/"
+cp $base_dir/artifacts/shaw/dbg.h "./$project/src/"
+cp $base_dir/artifacts/shaw/Makefile "./$project/"
 
