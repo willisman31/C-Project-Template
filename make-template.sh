c41b872ec2f2df47a56e5e3523355e992d688eee
willisman31 <jake22032@gmail.com>
2024-02-22 23:15:00 -0500

    Create error codes and simplify requirements for adding new templates

--- make-template.sh
+++ make-template.sh
@@ -12,6 +12,7 @@ usage: $0 [-h|--help] [-t] Template
 
 Options:
 
+-t		Specify a template to use
 --help		This help message
 - h		Basic usage message
 
@@ -20,16 +21,27 @@ Choose from one of the following templates:
 shaw		C project template from Shaw's "Learn C the Hard Way"
 github		C project template based on Github default filesystem
 
+Return code info:
+
+0	Success
+1	Error with template creation script
+2	Nonexistent template
+100	Help and usage
+
 EOF
 	fi
-	return 0
+	return 100
 }
-
-
 
 function template() {
 	wd=$(dirname $0)
 	scriptname="$wd/templates/$1-template.sh"
+
+	if [[ ! -f $scriptname ]]; then
+		echo "The template you tried to create does not current exist"
+		return 2
+	fi
+
 	/bin/sh $scriptname $1
 	if [[ $? -eq 0 ]]; then
 		echo "Created template"
@@ -40,19 +52,19 @@ function template() {
 	fi
 }
 
-function shaw-template() {
-	echo "Create Shaw template"
-}
-
 function main() {
-
 	if [[ $1 == '--help' || $1 == '-h' ]]; then
 		usage
+		export temp=$?
 	elif [[ $1 == '-t' && -n $2 ]]; then
 		shift ;
 		template $1
+		export temp=$?
 	fi
 
+	rc=${temp}
+	unset $temp
+	exit $rc
 }
 
 main $@
